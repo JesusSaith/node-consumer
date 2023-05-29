6cccbd0d9f479d9a8f19039c890bd892fa6b0f59
JesusSaith <yisuscond@gmail.com>
2023-05-29 06:56:20 -0600

    new2

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const cors = require ('cors')
-const express = require ('express')
-const {kafka} = require ('kafkajs')
+const cors = require('cors')
+const express = require('express')
+const { Kafka } = require('kafkajs')
 
-const kafka = new kafka({
-  clientId: 'my app',
+const kafka = new Kafka({
+  clientId: 'my-app',
   brokers: [
-    'my-kafka-0.my-kafka-headless.jesussaith.svc.cluster.local:9092'
-  ]
-  
-
+	  'my-kafka-0.my-kafka-headless.jesussaith.svc.cluster.local:9092'
+	  ]
 });
-
 
 const producer = kafka.producer()
 
@@ -21,9 +18,11 @@ app.options('*', cors());
 const port = 8080;
 
 app.get('/', (req, res, next) => {
-  res.send('kafka api - Saith');
+  res.send('kafka api - BryanVRe');
 });
 
+
+// topic username username=name
 const run = async (username) => {
 
     await producer.connect()
@@ -45,6 +44,57 @@ app.get('/like', (req, res, next) => {
   run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
 
 });
+//node - topic reaction - uId=userId, oId=objectId, rId=reactionId
+const ruun = async (uId, oId, rId) => {
+
+  await producer.connect()
+//    await producer.send()
+  await producer.send({
+    topic: 'reactions',
+    messages: [ 
+{ 
+  'value': `{ "userId": "${uId}",  "objectId": "${oId}", "reactionId": "${rId}"}` 
+  } 
+    ],
+  })
+ await producer.disconnect()
+}
+
+app.get('/reaction', (req, res, next) => {
+const uId = req.query.userId;
+const oId = req.query.objectId;
+const rId = req.query.reactionId;
+res.send({'userId:': uId, 'objectId': oId,'reactionId' : rId } );
+ruun(uId, oId, rId).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+//node - topic comments    uId=userId, oId=objectId, comment=message
+const r = async (uId, oId, comment) => {
+
+  await producer.connect()
+//    await producer.send()
+  await producer.send({
+    topic: 'comments',
+    messages: [ 
+  { 
+    'value': `{ "userId": "${uId}",  "objectId": "${oId}", "comment": "${comment}"}`
+  } 
+    ],
+  })
+ await producer.disconnect()
+}
+
+app.get('/comments', (req, res, next) => {
+const uId = req.query.userId;
+const oId = req.query.objectId;
+const comment = req.query.comment;
+res.send({'userId:': uId, 'objectId': oId,'comment' : comment} );
+r(uId, oId, comment).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+
 
 app.listen(port,  () => 
 	console.log('listening on port ' + port
